5d4ee6e1cbc0e12e981a0eafbdc18928e7b85660
AlexDor8 <st2022273@365.stucom.com>
2022-11-09 17:46:22 +0100

    explicación peticion asincrona a servidor

--- lib/src/dart_future.dart
+++ lib/src/dart_future.dart
@@ -5,12 +5,16 @@ library dart_future;
 
 import 'servicios.dart';
 
-
 export 'dart_future_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
 
+/**
+ * 1. Para hacer una petición asincrona a un servidor, le indicamos la url que corresponda con ese servidor. 
+ * Al ser asíncrono, el programa continuará ejecutandose al mismo tiempo. En el caso de qué queramos que nuestro 
+ * programa espere a que se realice esa petición utilizaremos la palabra reservada await
+ */
 
-void main(List<String> args) async{
+void main(List<String> args) async {
   await planetas();
 }
